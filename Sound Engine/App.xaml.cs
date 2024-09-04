978797e310f9eea7f1c65068fa30209551bfcb29
Hotwings <edcorgiat@gmail.com>
2024-09-04 13:07:45 -0500

    Created project and added ability to play sound when key is pressed

---
+++ Sound Engine/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sound_Engine
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
---
+++ Sound Engine/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Sound_Generator;
+
+using System.IO;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sound_Engine
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+
+
+
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			if (e.Key == Key.E)
+			{
+				using MemoryStream stream = new();
+				
+				SoundGenerator.Generate(stream);
+
+				using SoundPlayer player = new(stream);
+				player.Play();
+			}
+		}
+	}
+}
